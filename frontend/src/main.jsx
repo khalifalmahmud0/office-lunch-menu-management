5de8802e6ab402ba99ecfe127a0f37b999d52f90
khalifalmahmud0 <code.khalif@gmail.com>
2024-05-27 10:33:32 +0600

    feat: add react router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,13 @@ import App from "./components/app/App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
